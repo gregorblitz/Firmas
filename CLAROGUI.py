a7b8336e5cd2db87b902a33f8bdab5248ebcf085
oscardiazs <84478296+oscardiazs@users.noreply.github.com>
2022-09-28 11:26:12 -0500

    "Version 2 con boton comenzar que muestra resumen"

--- CLAROGUI.py
+++ CLAROGUI.py
@@ -28,6 +28,19 @@ def carpSalida():
     
 def comenzar():
     print('Hola')
+    
+def resumen(nombre,apellido,tipofirma,carpetaentrada,carpetasalida):
+    response = messagebox.askyesno("Verifique los datos "
+                                   ,"Nombre: "+nombre+"\n\nApellido: "+apellido
+                                   +"\n\nTipo de Firma: "+tipofirma
+                                   +"\n\nCarpeta de Entrada: "+carpetaentrada
+                                   +"\n\nCarpeta de Salida: "+carpetasalida)
+    #Label(root, text=response).pack()
+    
+    if response == 1:
+        Label(root, text="Stop it bro").pack()
+    else:
+        Label(root, text="What have u done").pack()
 
 #----------------------------------------
 
@@ -37,7 +50,7 @@ def comenzar():
 root = Tk()
 
 #Tamaño de la ventana
-root.geometry("940x500")
+root.geometry("940x400")
 root.resizable(False,False) #No permite hacer la ventana mas grande
 
 #Permite colocarle un titulo al programa
@@ -83,12 +96,15 @@ archivos = Frame(root, bd=2, relief=SOLID, padx=10, pady=10)
 
 #------------------Para ingresar Nombre--------
 Label(ConfigInicial, text="Ingrese Nombre: ", font=(fuente, 14)).grid(row=0, column=0, sticky=W, pady=10)
-entradaNombre = Entry(ConfigInicial, font=(fuente, 14)).grid(row=0, column=1, pady=10, padx=20)
+entradaNombre = Entry(ConfigInicial, font=(fuente, 14))
+entradaNombre.grid(row=0, column=1, pady=10, padx=20)
+
 #---------------------------------------------
 
 #------------------Para ingresar Apellido--------
 Label(ConfigInicial, text="Ingrese Apellido: ", font=(fuente, 14)).grid(row=1, column=0, pady=10)
-entradaApellido = Entry(ConfigInicial, font=(fuente, 14)).grid(row=1, column=1, pady=10, padx=20)
+entradaApellido = Entry(ConfigInicial, font=(fuente, 14))
+entradaApellido.grid(row=1, column=1, pady=10, padx=20)
 #---------------------------------------------
 
 
@@ -97,8 +113,9 @@ Label(ConfigInicial, text="Tipo de Firma: ", font=(fuente, 14)).grid(row=2, colu
 opcionesFirma = ["Manuscrito","Nombre"]
 Firmado = StringVar()
 Firmado.set(opcionesFirma[0])
-menuFirma = OptionMenu(ConfigInicial, Firmado,*opcionesFirma).grid(row=2, column=1, pady=10, padx=20)
-#menuFirma.config(width=10, font=(fuente, 14))
+menuFirma = OptionMenu(ConfigInicial, Firmado,*opcionesFirma)
+menuFirma.grid(row=2, column=1, pady=10, padx=20)
+menuFirma.config(width=10, font=(fuente, 14))
 #-----------------------------------------------
 
 #------------Cargar Firma---------------------
@@ -123,11 +140,13 @@ botonSalCarpeta = Button(archivos, text="Abrir",command = carpSalida,font=(fuent
 #Label(archivos, text="...", textvariable = EntCarpeta, font=(fuente, 14)).grid(row=3, column=0, sticky=W, pady=10) #Para probar nada mas
 #Label(archivos, text="...", textvariable = SalCarpeta, font=(fuente, 14)).grid(row=4, column=0, sticky=W, pady=10) #Para probar nada mas
 
-botonComenzar = Button(archivos, width=15, text='Comenzar', font=(fuente, 14), command=comenzar)
+botonComenzar = Button(archivos, width=15, text='Comenzar', font=(fuente, 14)
+                       , command=lambda: resumen(entradaNombre.get(),entradaApellido.get(),Firmado.get(),EntCarpeta.get(),SalCarpeta.get())) #command=lambda: button_click(1)
 botonComenzar.grid(row=7, column=1, pady=10, padx=20)
 
 #-------------------------------------------
 archivos.place(x=500, y=150)
+
 
 
 
